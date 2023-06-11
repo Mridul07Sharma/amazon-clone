460ddb99dc7779c6fea79ea45be3ad9f59477beb
Mridul07Sharma <mridul07sharma@gmail.com>
2023-06-11 14:23:32 +0530

    amazon-clone-change

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 const instance=axios.create({
-     baseURL:"https://us-central1-challenge-4b2b2.cloudfunctions.net/api"
-    //  "http://127.0.0.1:5001/clone-495fd/us-central1/api"
+     baseURL:
+    //  "https://us-central1-challenge-4b2b2.cloudfunctions.net/api"
+     "http://127.0.0.1:5001/clone-495fd/us-central1/api"
 
     // The API (Cloud Function) URL
 });
